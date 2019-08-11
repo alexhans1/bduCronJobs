b968427691c5c4a0525a0800042098c887bf99f5
alexhans <mail@alexanderhans.me>
2019-08-11 12:53:24 +0100

    made db connection depended on NODE_ENV

--- banking/index.js
+++ banking/index.js
@@ -1,12 +1,21 @@
 exports.dbConnection = {
   client: "mysql",
-  connection: {
-    host: "us-cdbr-iron-east-03.cleardb.net",
-    user: "bb41eedfd379a8",
-    password: process.env.clearDB_password,
-    database: "heroku_9b6f95eb7a9adf8",
-    charset: "utf8"
-  },
+  connection:
+    process.env.NODE_ENV === "production"
+      ? {
+          host: "us-cdbr-iron-east-03.cleardb.net",
+          user: "bb41eedfd379a8",
+          password: process.env.clearDB_password,
+          database: "heroku_9b6f95eb7a9adf8",
+          charset: "utf8"
+        }
+      : {
+          host: "localhost",
+          user: "root",
+          password: process.env.localDBPassword,
+          database: "heroku_9b6f95eb7a9adf8",
+          charset: "utf8"
+        },
   pool: {
     max: 10,
     min: 0
@@ -22,6 +31,7 @@ const transactionIdModelHelpers = require("./transactionIdModelHelpers");
 
 async function execute() {
   console.log("💵💵💵 Starting bank service 💵💵💵");
+
   const newTransactions = await finApiService.getNewTransactions();
   if (!newTransactions || !newTransactions.length)
     return console.info("No new transaction to process");
