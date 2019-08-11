f6257a55f95009de9183e473b02f52c02f73b557
alexhans <mail@alexanderhans.me>
2019-08-11 12:42:35 +0100

    pass db connection from index.js

--- banking/index.js
+++ banking/index.js
@@ -1,3 +1,21 @@
+exports.dbConnection = {
+  client: "mysql",
+  connection: {
+    host: "us-cdbr-iron-east-03.cleardb.net",
+    user: "bb41eedfd379a8",
+    password: process.env.clearDB_password,
+    database: "heroku_9b6f95eb7a9adf8",
+    charset: "utf8"
+  },
+  pool: {
+    max: 10,
+    min: 0
+  },
+  migrations: {
+    tableName: "knex_migrations"
+  }
+};
+
 const finApiService = require("./finApiService");
 const processTransactions = require("./processTransactions");
 const transactionIdModelHelpers = require("./transactionIdModelHelpers");
@@ -5,7 +23,8 @@ const transactionIdModelHelpers = require("./transactionIdModelHelpers");
 async function execute() {
   console.log("💵💵💵 Starting bank service 💵💵💵");
   const newTransactions = await finApiService.getNewTransactions();
-  if (!newTransactions || !newTransactions.length) return;
+  if (!newTransactions || !newTransactions.length)
+    return console.info("No new transaction to process");
 
   await transactionIdModelHelpers.saveTransactionIds(newTransactions);
   await processTransactions.processTransactions(newTransactions);
--- banking/transactionIdModelHelpers.js
+++ banking/transactionIdModelHelpers.js
@@ -1,20 +1,5 @@
-const knex = require("knex")({
-  client: "mysql",
-  connection: {
-    host: "us-cdbr-iron-east-03.cleardb.net",
-    user: "bb41eedfd379a8",
-    password: process.env.clearDB_password,
-    database: "heroku_9b6f95eb7a9adf8",
-    charset: "utf8"
-  },
-  pool: {
-    max: 10,
-    min: 0
-  },
-  migrations: {
-    tableName: "knex_migrations"
-  }
-}); // require knex query binder
+const { dbConnection } = require("./index");
+const knex = require("knex")(dbConnection); // require knex query binder
 const Bookshelf = require("bookshelf")(knex); // require Bookshelf ORM Framework
 
 // DEFINE MODELS
