3034296770ec87ab138b58c4f110d8c0a22476f2
alexhans <mail@alexanderhans.me>
2019-08-11 22:35:42 +0100

    removed conditional db connection

--- banking/index.js
+++ banking/index.js
@@ -1,21 +1,12 @@
 exports.dbConnection = {
   client: "mysql",
-  connection:
-    process.env.NODE_ENV === "production"
-      ? {
-          host: "us-cdbr-iron-east-03.cleardb.net",
-          user: "bb41eedfd379a8",
-          password: process.env.clearDB_password,
-          database: "heroku_9b6f95eb7a9adf8",
-          charset: "utf8"
-        }
-      : {
-          host: "localhost",
-          user: "root",
-          password: process.env.localDBPassword,
-          database: "heroku_9b6f95eb7a9adf8",
-          charset: "utf8"
-        },
+  connection: {
+    host: "us-cdbr-iron-east-03.cleardb.net",
+    user: "bb41eedfd379a8",
+    password: process.env.clearDB_password,
+    database: "heroku_9b6f95eb7a9adf8",
+    charset: "utf8"
+  },
   pool: {
     max: 10,
     min: 0
